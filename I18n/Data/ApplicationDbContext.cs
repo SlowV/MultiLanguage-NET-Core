e397f14d69206e0ec2f396534ccabfe0b564a342
SlowV <31509114+SlowV@users.noreply.github.com>
2018-12-09 22:44:12 +0700

    ...

---
+++ I18n/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using I18n.Models;
+
+namespace I18n.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<I18n.Models.User> User { get; set; }
+    }
+}
